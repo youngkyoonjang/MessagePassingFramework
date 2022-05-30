3bd18a6670fef08bb07309ef1673f4b63d5e4f7d
Cedric GOUBARD <c.goubard21@imperial.ac.uk>
2022-05-30 13:53:07 +0100

    Add check for nvidia-docker

--- 0_Do_necessary_settings_build_and_run.py
+++ 0_Do_necessary_settings_build_and_run.py
@@ -3,6 +3,10 @@ import sys
 
 my_python_path = sys.executable
 
+# If nvidia-docker is not installed, raise an error.
+if os.system("which nvidia-docker") == 256:
+    raise Exception("nvidia-docker is not installed. Please install it first.")
+
 os.system(my_python_path + ' -m pip install tqdm')
 os.system(my_python_path + ' 1_Download_files.py')
 os.system(my_python_path + ' 2_Switch_module_activation_and_assign_gpus.py --pose F:0 --object T:0 --hand F:0 --gaze F:0') ##Acvitate:T/F, gpu_id:0/1
@@ -10,4 +14,3 @@ os.system(my_python_path + ' 3_Switch_subscriber_activation.py --pose F --object
 os.system(my_python_path + ' 4_Switch_on_off_realsense_launch.py --realsense T') ##Acvitate:T/F
 os.system(my_python_path + ' 5_Replace_perception_folder_path_in_Makefile.py')
 os.system('make build && make run')
-
