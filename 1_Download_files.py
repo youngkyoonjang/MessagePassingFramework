8dcaa8fe9be9d77a6a26a272ae7c5a6c8ae2b7c1
Cedric GOUBARD <c.goubard21@imperial.ac.uk>
2022-05-30 17:37:45 +0100

    Clean up folder creation

--- 1_Download_files.py
+++ 1_Download_files.py
@@ -1,4 +1,9 @@
 import os
+from pathlib import Path
+import requests
+
+from rt_gene.download_tools import download_external_landmark_models, download_gaze_tensorflow_models
+
 
 def replace_the_first_line(file_name, line):
     """ Insert given string as a new line at the beginning of a file """
@@ -27,32 +32,32 @@ def exchange_strings(file_name, txt_to_be_replaced, with_txt_given):
             else:
                 f.write(line)
 
-os.system('mkdir ./tas_perception/models')
+Path('./tas_perception/models').mkdir(parents=True, exist_ok=True)
+
 
 models_directory_path = "./tas_perception/models"
 
 # Download MMDetection model and configuration files: YOLACT
-if(os.path.isfile(models_directory_path+'/yolact_r50_1x8_coco.py') == False):
+if not os.path.isfile(models_directory_path+'/yolact_r50_1x8_coco.py'):
     os.system('wget https://raw.githubusercontent.com/open-mmlab/mmdetection/master/configs/yolact/yolact_r50_1x8_coco.py -P ' + models_directory_path)
-if(os.path.isfile(models_directory_path+'/yolact_r101_1x8_coco.py') == False):
+if not os.path.isfile(models_directory_path+'/yolact_r101_1x8_coco.py'):
     os.system('wget https://raw.githubusercontent.com/open-mmlab/mmdetection/master/configs/yolact/yolact_r101_1x8_coco.py -P ' + models_directory_path)
-if(os.path.isfile(models_directory_path+'/yolact_r101_1x8_coco_20200908-4cbe9101.pth') == False):
+if not os.path.isfile(models_directory_path+'/yolact_r101_1x8_coco_20200908-4cbe9101.pth'):
     os.system('wget https://download.openmmlab.com/mmdetection/v2.0/yolact/yolact_r101_1x8_coco/yolact_r101_1x8_coco_20200908-4cbe9101.pth  -P ' + models_directory_path)
 
 # Download MMPose model and configuration files: HRNet (Topdown Heatmap + Hrnet on Coco -- pose_hrnet_w48 256x192)
-if(os.path.isfile(models_directory_path+'/hrnet_w48_coco_256x192.py') == False):
+if not os.path.isfile(models_directory_path+'/hrnet_w48_coco_256x192.py'):
     os.system('wget https://raw.githubusercontent.com/open-mmlab/mmpose/master/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w48_coco_256x192.py -P ' + models_directory_path)
     # replace_the_first_line(models_directory_path+'/hrnet_w48_coco_256x192.py', "_base_ = ['../_base_/datasets/coco.py']")
     exchange_strings(models_directory_path+'/hrnet_w48_coco_256x192.py', "../../../../_base_", "../_base_")
 
-if(os.path.isfile(models_directory_path+'/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth') == False):
+if not os.path.isfile(models_directory_path+'/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth'):
     os.system('wget https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth  -P ' + models_directory_path)
 
 
 # Download Hand-Object State Estimation model: FasterRCNN
 
 # -------------------------------------------------- Downloading googld drive file
-import requests
 
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
@@ -67,6 +72,7 @@ def download_file_from_google_drive(id, destination):
         response = session.get(URL, params = params, stream = True)
 
     save_response_content(response, destination)    
+
 
 def get_confirm_token(response):
     for key, value in response.cookies.items():
@@ -84,9 +90,10 @@ def save_response_content(response, destination):
                 f.write(chunk)
 # --------------------------------------------------
 
-os.system('mkdir ./tas_perception/hand_object/models')
+Path('./tas_perception/hand_object/models').mkdir(parents=True, exist_ok=True)
+
 hand_model_directory_path = "./tas_perception/hand_object/models"
-if(os.path.isfile(hand_model_directory_path+'/faster_rcnn_1_8_89999.pth') == False):
+if not os.path.isfile(hand_model_directory_path+'/faster_rcnn_1_8_89999.pth'):
     file_id = '166IM6CXA32f9L6V7-EMd9m8gin6TFpim'
     destination = hand_model_directory_path+'/faster_rcnn_1_8_89999.pth'
     print('./tas_perception/hand_object/models/faster_rcnn_1_8_89999.pth is downloading please wait until it is completed!')
@@ -94,9 +101,9 @@ if(os.path.isfile(hand_model_directory_path+'/faster_rcnn_1_8_89999.pth') == Fal
     print('./tas_perception/hand_object/models/faster_rcnn_1_8_89999.pth download complete!!')
 
 # Download rt_gene model /rt_gene_/model_nets folder
-os.system('mkdir ./rt_gene/model_nets')
-os.system('mkdir ./rt_gene/model_nets/SFD')
-os.system('mkdir ./rt_gene/model_nets/ThreeDDFA')
-from rt_gene.download_tools import download_external_landmark_models, download_gaze_tensorflow_models
+Path('/rt_gene/model_nets').mkdir(parents=True, exist_ok=True)
+Path('./rt_gene/model_nets/SFD').mkdir(parents=True, exist_ok=True)
+Path('./rt_gene/model_nets/ThreeDDFA').mkdir(parents=True, exist_ok=True)
+
 download_external_landmark_models()
 download_gaze_tensorflow_models()
