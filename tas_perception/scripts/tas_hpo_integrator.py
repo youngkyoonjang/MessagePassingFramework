4604988032897510f16c2c8c9c495bba4beed872
Youngkyoon Jang <youngkyoonjang@gmail.com>
2022-05-31 11:05:01 +0100

    exception added for tas_integrator
    accessing out of range when using callback

--- tas_perception/scripts/tas_hpo_integrator.py
+++ tas_perception/scripts/tas_hpo_integrator.py
@@ -234,108 +234,112 @@ class TAS_Integrator_YJ:
                 if(self.hand_json!=None and self.body_json!=None and self.instance_json!=None):
                     if(0<len(hand_states['hands'])):
                         for i in range(0, len(hand_states['hands'])):
-                            individual_hand_info_tmp = hand_states['hands'][i]
-                            hand_bbox = individual_hand_info_tmp['bbox']
-                            hand_state = individual_hand_info_tmp['state']
-                            hand_side = individual_hand_info_tmp['side_id']
+                            if(i< len(hand_states['hands'])):
+                                individual_hand_info_tmp = hand_states['hands'][i]
+                                hand_bbox = individual_hand_info_tmp['bbox']
+                                hand_state = individual_hand_info_tmp['state']
+                                hand_side = individual_hand_info_tmp['side_id']
 
-                            cv2.rectangle(self.img, (hand_bbox[0], hand_bbox[1]), (hand_bbox[2], hand_bbox[3]), color=self.hand_rgb[hand_side], thickness=4)
-                            cv2.rectangle(self.img, (hand_bbox[0], max(0, hand_bbox[1]-30)), (hand_bbox[0]+62, max(0, hand_bbox[1]-30)+30), color=self.hand_rgb[hand_side], thickness=4)
+                                cv2.rectangle(self.img, (hand_bbox[0], hand_bbox[1]), (hand_bbox[2], hand_bbox[3]), color=self.hand_rgb[hand_side], thickness=4)
+                                cv2.rectangle(self.img, (hand_bbox[0], max(0, hand_bbox[1]-30)), (hand_bbox[0]+62, max(0, hand_bbox[1]-30)+30), color=self.hand_rgb[hand_side], thickness=4)
 
-                            text = f'{self.side_map3[int(float(hand_side))]}-{self.state_map2[int(float(hand_state))]}'
-                            # org
-                            org = (hand_bbox[0]+6, max(0, hand_bbox[1])-8)
-                            # Using cv2.putText() method
-                            cv2.putText(self.img, text, org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+                                text = f'{self.side_map3[int(float(hand_side))]}-{self.state_map2[int(float(hand_state))]}'
+                                # org
+                                org = (hand_bbox[0]+6, max(0, hand_bbox[1])-8)
+                                # Using cv2.putText() method
+                                cv2.putText(self.img, text, org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
 
                         for i in range(0, len(hand_states['objects'])):
-                            individual_object_info_tmp = hand_states['objects'][i]
-                            object_bbox = individual_object_info_tmp['bbox']
+                            if(i< len(hand_states['objects'])):
+                                individual_object_info_tmp = hand_states['objects'][i]
+                                object_bbox = individual_object_info_tmp['bbox']
 
-                            cv2.rectangle(self.img, (object_bbox[0], object_bbox[1]), (object_bbox[2], object_bbox[3]), color=self.obj_rgb, thickness=4)
-                            cv2.rectangle(self.img, (object_bbox[0], max(0, object_bbox[1]-30)), (object_bbox[0]+32, max(0, object_bbox[1]-30)+30), color=self.obj_rgb, thickness=4)
+                                cv2.rectangle(self.img, (object_bbox[0], object_bbox[1]), (object_bbox[2], object_bbox[3]), color=self.obj_rgb, thickness=4)
+                                cv2.rectangle(self.img, (object_bbox[0], max(0, object_bbox[1]-30)), (object_bbox[0]+32, max(0, object_bbox[1]-30)+30), color=self.obj_rgb, thickness=4)
 
-                            text = f'O'
-                            # org
-                            org = (object_bbox[0]+5, max(0, object_bbox[1])-8)
-                            # Using cv2.putText() method
-                            cv2.putText(self.img, text, org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+                                text = f'O'
+                                # org
+                                org = (object_bbox[0]+5, max(0, object_bbox[1])-8)
+                                # Using cv2.putText() method
+                                cv2.putText(self.img, text, org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
                         #################
                 
                     
                     if(0<len(body)):
                         #########################################
                         for i in range(0, len(body)):
-                            bbox = body[i]['bbox']
-                            keypoints = body[i]['keypoints']
-                                                    
-                            cv2.rectangle(self.img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color=(0, 0, 255), thickness=1)
-                            
-                            #face
-                            for j in range(1, 3):
-                                cv2.line(self.img, (int(keypoints[j-1][0]), int(keypoints[j-1][1])), (int(keypoints[j][0]), int(keypoints[j][1])), (0,255,0), 1)
-                            cv2.line(self.img, (int(keypoints[0][0]), int(keypoints[0][1])), (int(keypoints[2][0]), int(keypoints[2][1])), (0,255,0), 1)
-                            
-                            ##two arms
-                            for j in range(2, 10, 2): 
-                                #right
-                                str_idx = j
-                                end_idx = j+2
-                                if(0.5<=keypoints[end_idx][2]):
-                                    cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
+                            if(i< len(body)):
+                                bbox = body[i]['bbox']
+                                keypoints = body[i]['keypoints']
+                                                        
+                                cv2.rectangle(self.img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color=(0, 0, 255), thickness=1)
                                 
-                                #left
-                                str_idx = j-1
-                                end_idx = (j+2)-1
-                                if(0.5<=keypoints[end_idx][2]):
-                                    cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
-                            
-                            ##two legs
-                            for j in range(12, 16, 2):
-                                #right
-                                str_idx = j
-                                end_idx = j+2
-                                if(0.5<=keypoints[end_idx][2]):
-                                    cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
+                                #face
+                                for j in range(1, 3):
+                                    cv2.line(self.img, (int(keypoints[j-1][0]), int(keypoints[j-1][1])), (int(keypoints[j][0]), int(keypoints[j][1])), (0,255,0), 1)
+                                cv2.line(self.img, (int(keypoints[0][0]), int(keypoints[0][1])), (int(keypoints[2][0]), int(keypoints[2][1])), (0,255,0), 1)
                                 
-                                #left
-                                str_idx = j-1
-                                end_idx = (j+2)-1
-                                if(0.5<=keypoints[end_idx][2]):
-                                    cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
-                            
-                            ##torso
-                            cv2.line(self.img, (int(keypoints[5][0]), int(keypoints[5][1])), (int(keypoints[6][0]), int(keypoints[6][1])), (0,255,0), 1)
-                            cv2.line(self.img, (int(keypoints[6][0]), int(keypoints[6][1])), (int(keypoints[12][0]), int(keypoints[12][1])), (0,255,0), 1)
-                            cv2.line(self.img, (int(keypoints[12][0]), int(keypoints[12][1])), (int(keypoints[11][0]), int(keypoints[11][1])), (0,255,0), 1)
-                            cv2.line(self.img, (int(keypoints[11][0]), int(keypoints[11][1])), (int(keypoints[5][0]), int(keypoints[5][1])), (0,255,0), 1)
-                            
-                            ## all joints
-                            for j in range(0, len(keypoints)):
-                                if(0.5<=keypoints[j][2]):
-                                    cv2.circle(self.img, (int(keypoints[j][0]), int(keypoints[j][1])), 3, (0,255,0), -1)
+                                ##two arms
+                                for j in range(2, 10, 2): 
+                                    #right
+                                    str_idx = j
+                                    end_idx = j+2
+                                    if(0.5<=keypoints[end_idx][2]):
+                                        cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
+                                    
+                                    #left
+                                    str_idx = j-1
+                                    end_idx = (j+2)-1
+                                    if(0.5<=keypoints[end_idx][2]):
+                                        cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
+                                
+                                ##two legs
+                                for j in range(12, 16, 2):
+                                    #right
+                                    str_idx = j
+                                    end_idx = j+2
+                                    if(0.5<=keypoints[end_idx][2]):
+                                        cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
+                                    
+                                    #left
+                                    str_idx = j-1
+                                    end_idx = (j+2)-1
+                                    if(0.5<=keypoints[end_idx][2]):
+                                        cv2.line(self.img, (int(keypoints[str_idx][0]), int(keypoints[str_idx][1])), (int(keypoints[end_idx][0]), int(keypoints[end_idx][1])), (0,255,0), 1)
+                                
+                                ##torso
+                                cv2.line(self.img, (int(keypoints[5][0]), int(keypoints[5][1])), (int(keypoints[6][0]), int(keypoints[6][1])), (0,255,0), 1)
+                                cv2.line(self.img, (int(keypoints[6][0]), int(keypoints[6][1])), (int(keypoints[12][0]), int(keypoints[12][1])), (0,255,0), 1)
+                                cv2.line(self.img, (int(keypoints[12][0]), int(keypoints[12][1])), (int(keypoints[11][0]), int(keypoints[11][1])), (0,255,0), 1)
+                                cv2.line(self.img, (int(keypoints[11][0]), int(keypoints[11][1])), (int(keypoints[5][0]), int(keypoints[5][1])), (0,255,0), 1)
+                                
+                                ## all joints
+                                for j in range(0, len(keypoints)):
+                                    if(0.5<=keypoints[j][2]):
+                                        cv2.circle(self.img, (int(keypoints[j][0]), int(keypoints[j][1])), 3, (0,255,0), -1)
                             
                     
                     if(0<len(instances)):
                         for i in range(0, len(instances['labels'])):
-                            label = instances['labels'][i]
-                            pred_box = instances['pred_boxes'][i]
-                            score = instances['scores'][i]
-                            pred_class = instances['pred_classes'][i]
-                            pred_contour = instances['pred_contours'][i]
-                            
-                            countour_mask_array = [np.array(pred_contour).astype(np.int32)]
-                            #create an empty image for contours                    
-                            image_binary = np.zeros((self.img.shape), np.uint8)
-                            # draw the contours on the empty image
-                            cv2.drawContours(self.img, countour_mask_array, -1, (0,255,0), 1)
-                            
-                            cv2.rectangle(self.img, (int(pred_box[0]), int(pred_box[1])), (int(pred_box[2]), int(pred_box[3])), color=(0, 0, 255), thickness=1)
-                            text = str(label) + ': ' +'{:.2f}'.format(score)
-                            # org
-                            org = (int(pred_box[0])+5, max(0, int(pred_box[1]))-8)
-                            # Using cv2.putText() method
-                            cv2.putText(self.img, text, org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+                            if(i < len(instances['labels'])):
+                                label = instances['labels'][i]
+                                pred_box = instances['pred_boxes'][i]
+                                score = instances['scores'][i]
+                                pred_class = instances['pred_classes'][i]
+                                pred_contour = instances['pred_contours'][i]
+                                
+                                countour_mask_array = [np.array(pred_contour).astype(np.int32)]
+                                #create an empty image for contours                    
+                                image_binary = np.zeros((self.img.shape), np.uint8)
+                                # draw the contours on the empty image
+                                cv2.drawContours(self.img, countour_mask_array, -1, (0,255,0), 1)
+                                
+                                cv2.rectangle(self.img, (int(pred_box[0]), int(pred_box[1])), (int(pred_box[2]), int(pred_box[3])), color=(0, 0, 255), thickness=1)
+                                text = str(label) + ': ' +'{:.2f}'.format(score)
+                                # org
+                                org = (int(pred_box[0])+5, max(0, int(pred_box[1]))-8)
+                                # Using cv2.putText() method
+                                cv2.putText(self.img, text, org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
                             
             if self.img is not None:
                 cv2.namedWindow('integrator_hand_pose_object')        # Create a named window
